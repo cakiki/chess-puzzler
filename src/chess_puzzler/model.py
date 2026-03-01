757070887f575988d9500c5a056addff53ccd77d
Christopher Akiki <christopher.akiki@gmail.com>
2026-03-01 23:29:42 +0100

    Add more models

--- src/chess_puzzler/model.py
+++ src/chess_puzzler/model.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
-from typing import Literal
+from typing import Literal, Optional
 from chess import Move, Color
+from chess.pgn import GameNode
+from chess.engine import Score
 
 TagKind = Literal[
     "advancedPawn",
@@ -74,3 +76,15 @@ class Puzzle:
 
     def moves_uci(self) -> list[str]:
         return [m.uci() for m in self.moves]
+    
+@dataclass
+class EngineMove:
+    move: Move
+    score: Score
+
+@dataclass
+class NextMovePair:
+    node: GameNode
+    winner: Color
+    best: EngineMove
+    second: Optional[EngineMove]
