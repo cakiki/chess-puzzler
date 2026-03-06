b011fd794e7fa397aa05451f7bddff2b5a80cca0
Christopher Akiki <christopher.akiki@gmail.com>
2026-03-06 22:26:17 +0100

    Get rid of static list of tags

--- src/chess_puzzler/model.py
+++ src/chess_puzzler/model.py
@@ -4,74 +4,13 @@ from chess import Move, Color
 from chess.pgn import GameNode
 from chess.engine import Score
 
-TagKind = Literal[
-    "advancedPawn",
-    "advantage",
-    "anastasiaMate",
-    "arabianMate",
-    "attackingF2F7",
-    "attraction",
-    "backRankMate",
-    "bishopEndgame",
-    "bodenMate",
-    "capturingDefender",
-    "castling",
-    "clearance",
-    "coercion",
-    "collinear",
-    "crushing",
-    "defensiveMove",
-    "discoveredAttack",
-    "deflection",
-    "doubleBishopMate",
-    "doubleCheck",
-    "dovetailMate",
-    "equality",
-    "enPassant",
-    "exposedKing",
-    "fork",
-    "hangingPiece",
-    "hookMate",
-    "interference",
-    "intermezzo",
-    "kingsideAttack",
-    "knightEndgame",
-    "long",
-    "mate",
-    "mateIn5",
-    "mateIn4",
-    "mateIn3",
-    "mateIn2",
-    "mateIn1",
-    "oneMove",
-    "overloading",
-    "pawnEndgame",
-    "pin",
-    "promotion",
-    "queenEndgame",
-    "queensideAttack",
-    "quietMove",
-    "rookEndgame",
-    "queenRookEndgame",
-    "sacrifice",
-    "short",
-    "simplification",
-    "skewer",
-    "smotheredMate",
-    "trappedPiece",
-    "underPromotion",
-    "veryLong",
-    "xRayAttack",
-    "zugzwang"
-]
-
 @dataclass
 class Puzzle:
     fen: str
     moves: list[Move]
     cp: int
     pov: Color
-    tags: list[TagKind] = field(default_factory=list)
+    tags: list[str] = field(default_factory=list)
     game_id: str = ""
 
     def moves_uci(self) -> list[str]:
